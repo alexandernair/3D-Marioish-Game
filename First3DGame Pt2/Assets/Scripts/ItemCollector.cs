488a0988149674a24ad1c320f2756265a0d047de
Alexander Nair <alexnair@me.com>
2021-12-28 15:11:52 -0600

    Completed first draft of capsule game

---
+++ First3DGame Pt2/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ItemCollector : MonoBehaviour
+{
+    int coins = 0;
+    [SerializeField] Text coinsText;
+    [SerializeField] AudioSource coinSound;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coins++;
+            coinSound.Play();
+            coinsText.text = "Coins: " + coins;
+        }
+    }
+}
